1c103d11b7ada61433b1548a41c69d7bde977bc0
cpalden <chimepalden@gmail.com>
2017-04-11 14:36:16 -0400

    Add files via upload

---
+++ indexFinal.php
@@ -0,0 +1,25 @@
+<?php
+include 'header.php';
+
+?>
+
+
+		<h1 class="text-center">Cypress System</h1>
+			<ul>
+				<li>
+					<a href="about.php">About</a>
+				</li>
+				<li>
+					<a href="report.php">Report</a>
+				</li>
+				<li>
+					<a href="contact.php">Contact</a>
+				</li>
+			</ul>
+		
+		
+		
+		
+		<footer class="text-right"><a id="footer"> FAQ </a></footer>
+	</body>
+</html>
